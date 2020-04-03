34d3e2fe3fef20553ae6adebb121bed963af7229
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-04-03 10:34:28 -0400

    Delete unused CMS files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,18 +3,8 @@ import uploadcare from 'netlify-cms-media-library-uploadcare';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import IndexPagePreview from './preview-templates/IndexPagePreview';
-// import MeetTheOwnerPagePreview from './preview-templates/MeetTheOwnerPagePreview';
-// import PastriesPagePreview from './preview-templates/PastriesPagePreview';
-// import SimplePagePreview from './preview-templates/SimplePagePreview';
-// import SnapshotsPagePreview from './preview-templates/SnapshotsPagePreview';
-// import TimelinePagePreview from './preview-templates/TimelinePagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
-// CMS.registerPreviewTemplate('meet-the-owner', MeetTheOwnerPagePreview);
-// CMS.registerPreviewTemplate('pastries', PastriesPagePreview);
-// CMS.registerPreviewTemplate('simple', SimplePagePreview);
-// CMS.registerPreviewTemplate('snapshots', SnapshotsPagePreview);
-// CMS.registerPreviewTemplate('timeline', TimelinePagePreview);
